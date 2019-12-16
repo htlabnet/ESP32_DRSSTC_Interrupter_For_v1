832daa326a8036415bf5a03b78f0f5a099a30aea
pcjpnet <pcjpnet@users.noreply.github.com>
2019-12-16 20:33:16 +0900

    First Commit

---
+++ Firmware/HTLAB.NET_ESP32_DRSSTC_Interrupter/HTLAB.NET_ESP32_DRSSTC_Interrupter.ino
@@ -0,0 +1,61 @@
+//
+// #################################################
+//
+//    HTLAB.NET ESP32 DRSSTC Interrupter
+//      https://htlab.net/electronics/teslacoil/
+//
+//    Copyright (C) 2019 - 2020
+//      Hideto Kikuchi / PJ (@pcjpnet) - http://pc-jp.net/
+//      Tsukuba Science Inc. - http://www.tsukuba-kagaku.co.jp/
+//
+//    !!!!! NOT ALLOWED COMMERCIAL USE !!!!!
+//
+// #################################################
+//
+
+//
+// ########## Compatible Boards ##########
+//    - ESP32-DevKitC
+//
+
+//
+// ########## ESP32 Pin Assignments ##########
+//  IO0 - NONE
+//  IO2 - HSPI CS [SS] (ADC1 CS)
+//  IO4 - HSPI CS [SS] (ADC2 CS)
+//  IO5 - VSPI CS [SS] (SD DAT3)
+//  IO12 - HSPI IQ [MISO] (ADC1, ADC2, LCD IOExp)
+//  IO13 - HSPI ID [MOSI] (ADC1, ADC2, LCD IOExp)
+//  IO14 - HSPI CLK [SCK] (ADC1, ADC2, LCD IOExp)
+//  IO15 - HSPI CS [SS] (LCD IOExp CS)
+//  IO16 - UART2 RXD (MIDI IN)
+//  IO17 - UART2 TXD (MIDI OUT)
+//  IO18 - VSPI CLK [SCK] (SD CLK)
+//  IO19 - VSPI IQ [MISO] (SD DAT0)
+//  IO21 - PIEZO SPEAKER
+//  IO22 - UART0 MODE OUT (MIDI/UART)
+//  IO23 - VSPI ID [MOSI] (SD CMD)
+//  IO25 - DAC1 (LCD CONTRAST)
+//  IO26 - OUT
+//  IO27 - OUT
+//  IO32 - OUT
+//  IO33 - OUT
+//  IO34 - Internal ADC1 CH6 (BATT VOLTAGE)
+//  IO35 - NONE
+//
+
+//
+// ########## Require Libraries ##########
+//    - MIDI Library 4.3
+//
+
+
+void setup() {
+
+}
+
+
+
+void loop() {
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ESP32_DRSSTC_Interrupter_For_v1
+# HTLAB.NET ESP32 DRSSTC Interrupter Ver 1.0
+
+
+
+
+https://htlab.net/electronics/teslacoil/
+
+
